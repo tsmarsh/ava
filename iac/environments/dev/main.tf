d7d3ac92a830c98643364b7a362a3c2e3844bc48
Telegram Docker Build <td.build@tailoredshapes.com>
2023-12-20 23:28:45 +0000

    Update Docker image tag in Terraform to 7281926550

--- iac/environments/dev/main.tf
+++ iac/environments/dev/main.tf
@@ -20,7 +20,7 @@ module "containers" {
   depends_on = [module.infra, module.database]
   source = "../../modules/azure_container"
   connection_string = module.database.connection_string
-  image = "tsmarsh/ava-telegram:0.0.2"
+  image = "tsmarsh/ava-telegram:7281926550
   resource_group = module.infra.resource_group
   security_group = module.infra.security_group
   location = module.infra.location
